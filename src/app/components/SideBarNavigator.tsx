39c38a15c1f073f768054627abd6600ac338174e
Puwit <zeroed312@gmail.com>
2025-10-26 14:21:39 +0700

    Add: profile navigation

--- src/app/components/SideBarNavigator.tsx
+++ src/app/components/SideBarNavigator.tsx
@@ -10,17 +10,19 @@ import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import EventNoteRoundedIcon from '@mui/icons-material/EventNoteRounded';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import { red } from '@mui/material/colors';
+import Person2RoundedIcon from '@mui/icons-material/Person2Rounded';
 import { Button } from '@mui/material';
 import Link from "next/link";
+import { icon } from 'leaflet';
 
 const drawerWidth = 240;
 
@@ -49,70 +51,71 @@ interface AppBarProps extends MuiAppBarProps {
 //     )
 // }
 
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  variants: [
-    {
-      props: ({ open }) => open,
-      style: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: `${drawerWidth}px`,
-        transition: theme.transitions.create(['margin', 'width'], {
-          easing: theme.transitions.easing.easeOut,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-        backgroundColor: "transparent",
-      },
-    },
-  ],
-}));
-
-const ButtonDrawer = styled(Button, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  variants: [
-    {
-      props: ({ open }) => open,
-      style: {
-        // width: `calc(100% - ${drawerWidth}px)`,
-        position: "fixed",
-        marginLeft: `${drawerWidth}px`,
-        transition: theme.transitions.create(['margin', 'width'], {
+  const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+  })<AppBarProps>(({ theme }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    variants: [
+      {
+        props: ({ open }) => open,
+        style: {
+          width: `calc(100% - ${drawerWidth}px)`,
+          marginLeft: `${drawerWidth}px`,
+          transition: theme.transitions.create(['margin', 'width'], {
             easing: theme.transitions.easing.easeOut,
             duration: theme.transitions.duration.enteringScreen,
-        }),
-        backgroundColor: "transparent",
+          }),
+          backgroundColor: "transparent",
+        },
       },
-    },
-  ],
-}));
+    ],
+  }));
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-  justifyContent: 'flex-end',
-}));
+  const ButtonDrawer = styled(Button, {
+    shouldForwardProp: (prop) => prop !== 'open',
+  })<AppBarProps>(({ theme }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    variants: [
+      {
+        props: ({ open }) => open,
+        style: {
+          // width: `calc(100% - ${drawerWidth}px)`,
+          position: "fixed",
+          marginLeft: `${drawerWidth}px`,
+          transition: theme.transitions.create(['margin', 'width'], {
+              easing: theme.transitions.easing.easeOut,
+              duration: theme.transitions.duration.enteringScreen,
+          }),
+          backgroundColor: "transparent",
+        },
+      },
+    ],
+  }));
+
+  const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  }));
 
 
 export default function SideBarNavigator() {
   const theme = useTheme();
   const [open, setOpen] = React.useState(true);
   const listItem = [
-    { name: 'Home', path: '/' },
-    { name: 'Activity Lobby', path: '/activity-lobby' },
-    { name: 'Create an Activity', path: '/create-activity' },
+    { name: 'Home', path: '/' , icon: <HomeRoundedIcon />},
+    { name: 'Activity Lobby', path: '/activity-lobby' , icon: <EventNoteRoundedIcon />},
+    { name: 'Create an Activity', path: '/createactivity' , icon: <AddRoundedIcon />},
+    { name: 'Profile', path:'/profile',icon: <Person2RoundedIcon />}
   ]
 
   const handleDrawer = () => {
@@ -154,25 +157,68 @@ export default function SideBarNavigator() {
         
         {/* <OpenDrawerButton theme={theme}/> */}
 
+        {!open && (
+        <IconButton
+          onClick={handleDrawerOpen}
+          sx={{
+            position: "fixed",
+            top: "50%",
+            left: 16,
+            transform: "translateY(-50%)",
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+            backgroundColor: theme.palette.primary.main,
+            boxShadow: 2,
+            "&:hover": {
+              backgroundColor: theme.palette.info.main,
+            },
+          }}
+        >
+          <ChevronRightIcon
+          sx={{color: theme.palette.background.paper}}
+           />
+        </IconButton>
+      )}
+
         <Drawer
             sx={{
-            width: drawerWidth,
+            width: open ? drawerWidth : 0,
             flexShrink: 0,
             '& .MuiDrawer-paper': {
-                width: drawerWidth,
-                boxSizing: 'border-box',
+                width: open ? drawerWidth : 0,
+                backgroundColor: theme.palette.primary.main,
+                color: theme.palette.background.paper,
+                boxSizing: 'border-box',      
             },
             }}
             variant="persistent"
             anchor="left"
             open={open}
         >
-            
-            <DrawerHeader>
-                <IconButton onClick={handleDrawerClose}>
-                    {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                </IconButton>
-            </DrawerHeader>
+
+
+          <DrawerHeader
+          sx={{ bgcolor: theme.palette.primary.main }}
+          >
+
+          </DrawerHeader>
+
+            <IconButton onClick={handleDrawerClose}
+            sx={{
+              position: "fixed",
+              top: "50%",
+              left: drawerWidth - 2,
+              transform: "translateY(-50%)",
+              zIndex: (theme) => theme.zIndex.drawer + 1,
+              borderRadius: "0 10px 10px 0",
+              backgroundColor: theme.palette.primary.main,
+              boxShadow: 2,
+              "&:hover": {
+                backgroundColor: theme.palette.info.main
+              },
+            }}
+            >
+                {theme.direction === 'ltr' ? <ChevronLeftIcon sx={{color: theme.palette.background.paper}} /> : <ChevronRightIcon />}
+            </IconButton>
 
             <Divider />
             
@@ -184,7 +230,23 @@ export default function SideBarNavigator() {
                     passHref
                     style={{ textDecoration: 'none', color: 'inherit', width: '100%' }}
                   >
-                    <ListItemButton>
+                    <ListItemButton
+                      sx={{
+                        color: theme.palette.primary.contrastText,    // normal text color
+                        borderRadius:5,         
+                        "&:hover": {
+                          backgroundColor: theme.palette.background.paper,     // hover background
+                          color: theme.palette.primary.main,           // hover text color
+                          borderRadius: 5,       
+                          "& .MuiListItemIcon-root": {
+                            color: theme.palette.primary.main,       // hover icon color
+                          },
+                        },
+                      }}
+                    >
+                      <ListItemIcon sx={{ color: theme.palette.background.paper }}>
+                        {item.icon}
+                      </ListItemIcon>
                       <ListItemText primary={item.name} />
                     </ListItemButton>
                   </Link>
